21ec49f5dba114847e9e2fd0829febe891f011fb
Colin Lin <colincool100@gmail.com>
2020-02-19 14:18:26 -0500

    oops

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(db, { useNewUrlParser: true,useUnifiedTopology: true })
     if(process.env.NODE_ENV == 'production'){
 
         //set static folder
-        app.user(express.static('client/build'));
+        app.use(express.static('client/build'));
         app.get('*',(req,res)=>{
             res.sendFile(path.resolve(__dirname,'client','build','index.html'));
 
