0e0ffbc54998f0da9397f9a7b90d78874b69aa97
Colin Lin <colincool100@gmail.com>
2020-02-19 14:51:06 -0500

    mobilefix

--- client/src/store.js
+++ client/src/store.js
@@ -4,9 +4,17 @@ import rootReducer from './reducers';
 
 const initialState ={};
 const middleware =[thunk];
+/*
 const store = createStore(rootReducer,initialState,compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+));*/
+
+const devTools =
+  process.env.NODE_ENV === "production"
+    ? applyMiddleware(...middleware)
+    : compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, initialState, devTools);
+
 export default store;
 
